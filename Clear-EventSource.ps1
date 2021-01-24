73ce3de7ad09f9418a61982db32ac00bff793af2
James Brundage <james.brundage@start-automating.com>
2021-01-24 15:45:20 -0800

    Clear-EventSource:  Enabling bulk remove of event sources.

---
+++ Clear-EventSource.ps1
@@ -0,0 +1,36 @@
+﻿function Clear-EventSource
+{
+    <#
+    .Synopsis
+        Clears event source subscriptions
+    .Description
+        Clears any active subscriptions for any event source.
+    .Example
+        Clear-EventSource
+    .Link
+        Get-EventSource
+    #>
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param(
+    # The name of the event source.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string[]]
+    $Name)
+
+    process {
+        #region Determine Event Sources
+        $parameterCopy = @{} + $PSBoundParameters
+        $null = $parameterCopy.Remove('WhatIf')
+        $eventSources = Get-EventSource @parameterCopy -Subscription
+        if ($WhatIfPreference) {
+            $eventSources
+            return
+        }
+        #endregion Determine Event Sources
+        #region Unregister
+        if ($PSCmdlet.ShouldProcess("Clear event sources $($Name -join ',')")) {
+            $eventSources | Unregister-Event
+        }
+        #endregion Unregister
+    }
+}
