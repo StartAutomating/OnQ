df6de26bf6223e3934c76a348f07718f6c9c2bb4
James Brundage <james.brundage@start-automating.com>
2021-01-24 15:49:21 -0800

    @Process: Run scripts when a process exists, or gets new -StandardError or -StandardOutput.

---
+++ @Process.ps1
@@ -0,0 +1,47 @@
+﻿<#
+.Synopsis
+    Watches a process.
+.Description
+    Watches a process.
+
+    If -Exit is passed, watches for process exit.
+
+    If -Output is passed, watches for process output
+
+    If -Error is passed, watched for process error
+#>
+param(
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[int]
+$ProcessID,
+
+[Parameter(ValueFromPipelineByPropertyName)]
+[switch]
+$Exit,
+
+[switch]
+$StandardOutput,
+
+[switch]
+$StandardError
+)
+
+$eventNames = @(
+    if ($Exit) {
+        "Exited"
+    }
+    if ($StandardOutput) {
+        "OutputDataReceived"
+    }
+    if ($StandardError) {
+        "ErrorDataReceived"
+    }
+)
+
+if ($eventNames) {
+    Get-Process -Id $ProcessID |
+        Add-Member EventName $eventNames -Force -PassThru
+} else {
+    Get-Process -Id $ProcessID |
+        Add-Member EventName "Exited" -Force -PassThru
+}
