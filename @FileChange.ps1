b4362eabe37eab5e53ca6942428b35d3d2e7d9d5
James Brundage <james.brundage@start-automating.com>
2021-01-24 17:51:19 -0800

    @FileChange: Adding Parameter help and making -Filter work.

--- @FileChange.ps1
+++ @FileChange.ps1
@@ -9,19 +9,23 @@
 [Diagnostics.Tracing.EventSource(Name='Deleted')]
 [Diagnostics.Tracing.EventSource(Name='Renamed')]
 param(
+# The path to the file or directory
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('Fullname')]
 [string]
 $FilePath = "$pwd",
 
+# A wildcard filter describing the names of files to watch
 [Parameter(ValueFromPipelineByPropertyName)]
 [string]
 $FileFilter,
 
+# A notify filter describing the file changes that should raise events.
 [Parameter(ValueFromPipelineByPropertyName)]
 [IO.NotifyFilters[]]
 $NotifyFilter = @("FileName", "DirectoryName", "LastWrite"),
 
+# If set, will include subdirectories in the watcher.
 [Alias('InludeSubsdirectory','InludeSubsdirectories')]
 [switch]
 $Recurse
@@ -46,6 +50,7 @@ process {
 
     $fileSystemWatcher.EnableRaisingEvents =$true
     $fileSystemWatcher.IncludeSubdirectories = $Recurse
+    $fileSystemWatcher.Filter = $FileFilter
     $combinedNotifyFilter = 0
     foreach ($n in $NotifyFilter) {
         $combinedNotifyFilter = $combinedNotifyFilter -bor $n
