c1b0b07d37482d47fa9f1a2ff2f87063738cee07
James Brundage <james.brundage@start-automating.com>
2021-03-03 12:55:32 -0800

    Adding New Event Source:  UDP

---
+++ EventSources/@UDP.ps1
@@ -0,0 +1,56 @@
+﻿<#
+
+#>
+param(
+[Parameter(ValueFromPipelineByPropertyName)]
+[Net.IPAddress]
+$IPAddress = [Net.IPAddress]::Any,
+
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[int]
+$Port,
+
+[Parameter(ValueFromPipelineByPropertyName)]
+[Text.Encoding]
+$Encoding
+)
+
+$UdpBackgroundJob = {
+    param($IPAddress, $port, $encoding)
+    
+    $udpSvr=  [Net.Sockets.UdpClient]::new()
+    $endpoint = [Net.IpEndpoint]::new($IPAddress, $Port)
+
+    try {
+        $udpSvr.Client.Bind($endpoint)
+    } catch  {
+        Write-Error -Message $_.Message -Exception $_
+        return
+    }
+    $eventSourceId = "UDP.${IPAddress}:$port"
+    Register-EngineEvent -SourceIdentifier $eventSourceId -Forward
+    if ($encoding) {
+        $RealEncoding  = [Text.Encoding]::GetEncoding($encoding.BodyName)
+        if (-not $RealEncoding) {
+            throw "Could not find $($encoding | Out-String)"
+        }
+    }
+    while ($true) {
+        $packet = $udpSvr.Receive([ref]$endpoint)
+
+        if ($RealEncoding) {            
+            New-Event -Sender $IPAddress -MessageData $RealEncoding.GetString($packet) -SourceIdentifier $eventSourceId |
+                Out-Null
+        } else {
+            New-Event -Sender $IPAddress -MessageData $packet -SourceIdentifier $eventSourceId  | Out-Null
+        }
+        
+    }
+}
+
+$startedJob = Start-Job -ScriptBlock $UdpBackgroundJob -ArgumentList $IPAddress, $port, $Encoding
+@{
+    SourceIdentifier = "UDP.${IPAddress}:$port"
+}
+
+
